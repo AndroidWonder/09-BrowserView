476b807c9452a797de178af3038e6497cf4f5528
Jim Pepe <jim.pepe@verizon.net>
2017-04-29 20:56:31 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/browserview/BrowserView.java
@@ -0,0 +1,51 @@
+/*
+ * Use a WebView widget to display a web page.
+ * Enter URL, click button or press Enter key.
+ */
+package com.course.example.browserview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class BrowserView extends Activity {
+   private EditText urlText;
+   private Button goButton;
+   private WebView webView;
+   
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      // ...
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+
+      // Get a handle to all user interface elements
+      urlText = (EditText) findViewById(R.id.url_field);
+      goButton = (Button) findViewById(R.id.go_button);
+      webView = (WebView) findViewById(R.id.web_view);
+      webView.getSettings().setJavaScriptEnabled(true);
+
+      // Set button to open browser
+      goButton.setOnClickListener(new OnClickListener() {
+         public void onClick(View view) {
+        	 webView.loadUrl(urlText.getText().toString());
+         }
+      });
+      urlText.setOnKeyListener(new OnKeyListener() {
+         public boolean onKey(View view, int keyCode, KeyEvent event) {
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            	webView.loadUrl(urlText.getText().toString());
+               return true;
+            }
+            return false;
+         }
+      });
+   }
+
+}
